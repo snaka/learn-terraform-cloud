660d5b9bd550ba7a8026d9ee1f01a70fc9a07dc6
S.Nakamatsu <19329+snaka@users.noreply.github.com>
2022-10-29 17:59:07 +0900

    Rmove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,14 @@
 terraform {
 
+/*
   cloud {
-    organization = "organization-name"
+    organization = "snaka-terraform-test"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
+*/
 
   required_providers {
     aws = {
